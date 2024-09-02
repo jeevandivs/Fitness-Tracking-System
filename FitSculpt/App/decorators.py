3d85e0046c12c53067602a97e6ad29048455f303
jeevandominic <jeevandominic90@gmail.com>
2024-09-02 10:11:58 +0530

    1.1

--- FitSculpt/App/decorators.py
+++ FitSculpt/App/decorators.py
@@ -11,3 +11,13 @@ def custom_login_required(view_func):
             return redirect('login')
     return _wrapped_view
 
+def fm_custom_login_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if 'fm_user_id' in request.session:
+            return view_func(request, *args, **kwargs)
+        else:
+            print(f"User not authenticated. Redirecting to fitness manager login.")
+            return redirect('fm_login')
+    return _wrapped_view
+
