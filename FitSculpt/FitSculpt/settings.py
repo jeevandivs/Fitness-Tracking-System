554f92fa6403db3165c8da61207eed47d8aa33d2
jeevandominic <jeevandominic90@gmail.com>
2024-11-06 16:38:05 +0530

    'commit 2.1'

--- FitSculpt/FitSculpt/settings.py
+++ FitSculpt/FitSculpt/settings.py
@@ -113,6 +113,9 @@ DATABASES = {
         'PASSWORD': '16f9d85cdd3c08e3635bc838e51fe1c951e58e11',
         'HOST': 'rb51n.h.filess.io',
         'PORT': '3307',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 AUTHENTICATION_BACKENDS = (
